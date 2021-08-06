0224732eacf1359c32f99ea86c840d46d5d54eac
Nicolas Venzmer <nicovenz@hotmail.com>
2021-08-06 16:08:37 -0300

    Changed back unused values.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -6,7 +6,7 @@ import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
 
-  const [{ basket }] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="subtotal">
